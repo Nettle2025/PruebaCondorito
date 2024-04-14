b6e907962b91db44d77874c85e675ed95da38881
Nettle2025 <pablo.nettle@inacapmail.cl>
2024-04-14 17:39:05 +0000

    Evaluacion 1

---
+++ #Solicitar informacion de usuario.py
@@ -0,0 +1,12 @@
+#Solicitar informacion de usuario
+nombre=input("Nombre:")
+apellido=input("Apellido:")
+seccion=input("Codigo de Seccion:")
+sede=input("Sede:")
+
+# Resultados
+print("\nInformacion Ingresada")
+print(f"Nombre:{nombre}")
+print(f"Apellido:{apellido}")
+print(f"Codigo-Seccion:{seccion}")
+print(f"Sede:{sede}")
